02ec642ced19bd5a2bbf4fa4cafff7a2f1a0dd02
benaor <benjamingirard25@gmail.com>
2020-09-13 22:19:06 +0200

    add anotation and routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="homePage")
+     * @Route("/", name="homePage")
      */
     public function index()
     {
