b520fb1e4ffbc7a9cd035999d56318bab8614489
benaor <benjamingirard25@gmail.com>
2020-09-13 21:58:22 +0200

    create controller : HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="homePage")
+     */
+    public function index()
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
