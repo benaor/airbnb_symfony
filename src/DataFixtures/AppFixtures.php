f5608f0eb2af045feddce37c3914329396cad007
benaor <benjamingirard25@gmail.com>
2020-09-16 00:52:18 +0200

    create fake data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ad;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $ad = new Ad();
 
+        $ad->setTitle("titre de l\'annonce")
+            ->setSlug("titre-de-l-annonce")
+            ->setCoverImage("http://placehold.it/1000x300")
+            ->setIntroduction("this is an amazing introduction")
+            ->setContent("<p>Hi, i'm very rich content</p>")
+            ->setPrice(80)
+            ->setRooms(3);
+
+        $manager->persist($ad);
         $manager->flush();
     }
 }
