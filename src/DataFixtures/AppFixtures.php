27da7e58c7b302a6bb3d0376941a937d8d8873fc
benaor <benjamingirard25@gmail.com>
2020-09-16 23:48:36 +0200

    add  in datafixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,23 +5,31 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('FR-fr');
         
         for($i=1 ; $i < 40; $i++){ 
 
             //new Annonce
             $ad = new Ad();
             
+            //create faker data
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000, 350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>';
+
             //Add property of this ad
-            $ad->setTitle("title number ".$i)
+            $ad->setTitle($title)
                 ->setSlug("titre-de-l-annonce".$i)
-                ->setCoverImage("http://placehold.it/1000x300")
-                ->setIntroduction("this is an amazing introduction")
-                ->setContent("<p>Hi, i'm very rich content</p>")
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
                 ->setPrice(mt_rand(20, 500))
                 ->setRooms(mt_rand(0, 8));
     
