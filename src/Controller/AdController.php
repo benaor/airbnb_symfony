94d431476002f769d0a84656cc2e7ee4e417f3eb
benaor <benjamingirard25@gmail.com>
2020-09-17 23:12:02 +0200

    create adController

---
+++ src/Controller/AdController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdController extends AbstractController
+{
+    /**
+     * @Route("/ad", name="ad")
+     */
+    public function index()
+    {
+        return $this->render('ad/index.html.twig', [
+            'controller_name' => 'AdController',
+        ]);
+    }
+}
