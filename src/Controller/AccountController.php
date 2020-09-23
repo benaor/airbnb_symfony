ba51e081eaa031ad62f58b0f03fcb2cb0179b350
benaor <benjamingirard25@gmail.com>
2020-09-24 00:57:06 +0200

    correction orthographe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,10 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     /**
+     * For user login
+     * 
      * @Route("/login", name="account_login")
+     * 
+     * @return Response
      */
     public function login()
     {
         return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * For user logout
+     * 
+     * @Route("/logout", name="account_logout")
+     * 
+     * @return Response
+     */
+    public function logout()
+    {
     }
 }
