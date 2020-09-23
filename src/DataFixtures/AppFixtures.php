1f98c5b7e6c11828757d35152b6f2e332350c623
benaor <benjamingirard25@gmail.com>
2020-09-23 22:19:29 +0200

    generate faker 'User' with fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Faker\Factory;
@@ -13,7 +14,26 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+        
+        //Manage they User
+        $users = [];
 
+        for ($i=1; $i < 10; $i++) { 
+            $user = new User();
+
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                ->setHash('password')
+                ;
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        //Manage they Ads
         for ($i = 1; $i < 40; $i++) {
 
             //new Annonce
@@ -24,6 +44,7 @@ class AppFixtures extends Fixture
             $coverImage     = $faker->imageUrl(1000, 350);
             $introduction   = $faker->paragraph(2);
             $content        = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+            $user           = $users[mt_rand(0, count($users) -1)];
 
             //Add property of this ad
             $ad->setTitle($title)
@@ -31,7 +52,8 @@ class AppFixtures extends Fixture
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(20, 500))
-                ->setRooms(mt_rand(0, 8));
+                ->setRooms(mt_rand(0, 8))
+                ->setAuthor($user);
 
             for ($j=1; $j < mt_rand(2, 5); $j++) { 
                 $image      = new Image();
