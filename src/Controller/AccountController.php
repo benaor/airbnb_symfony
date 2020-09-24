32898819daa893438f9340b0ace7253f08c9cb43
benaor <benjamingirard25@gmail.com>
2020-09-24 12:55:27 +0200

    add authentificationUtils

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
@@ -14,9 +15,18 @@ class AccountController extends AbstractController
      * 
      * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('account/login.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ]);
     }
 
     /**
