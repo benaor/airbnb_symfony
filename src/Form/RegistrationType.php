5e8c338bfe325e5dadebceba0b6ac18b062b434d
benaor <benjamingirard25@gmail.com>
2020-09-25 23:33:47 +0200

    configure RegistrationType.php

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,21 +6,43 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RegistrationType extends AbstractType
 {
+
+    /**
+     * For configure base of a input
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array  $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = [])
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('picture')
-            ->add('hash')
-            ->add('introduction')
-            ->add('description')
-            ->add('slug')
-        ;
+            ->add('firstName', TextType::class, $this->getConfiguration("Prenom", "Votre prénom ..."))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom ..."))
+            ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre Email ..."))
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "URL de votre photo de profil (avatar) ..."))
+            ->add('hash', PasswordType::class, $this->getConfiguration("Mot de passe", "Choisissez un mot de passe solide (minimum 6 caractères"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Présentez vous en quelques mots ..."))
+            ->add('description', TextareaType ::class, $this->getConfiguration("Description", "Décrivez vous en détails ..."));
     }
 
     public function configureOptions(OptionsResolver $resolver)
