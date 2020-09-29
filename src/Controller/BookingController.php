842671a6d01a25c5c15e473c3d3884b0a4e1884f
benaor <benjamingirard25@gmail.com>
2020-09-29 23:31:52 +0200

    rename booking/index.html.twig to booking/book.html.twig and create function book() in BookingController

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Entity\Booking;
+use App\Form\BookingType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends AbstractController
 {
     /**
-     * @Route("/booking", name="booking")
+     * @Route("/ads/{slug}/book", name="booking_create")
      */
-    public function index()
+    public function book(Ad $ad)
     {
-        return $this->render('booking/index.html.twig', [
-            'controller_name' => 'BookingController',
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+
+        return $this->render('booking/book.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
