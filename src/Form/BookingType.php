fdc2bc65a8aaf6eced153f4ee336ed13463e0b69
benaor <benjamingirard25@gmail.com>
2020-09-29 23:19:35 +0200

    create BookingType (form for reservation)

---
+++ src/Form/BookingType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate')
+            ->add('endDate')
+            ->add('createdAt')
+            ->add('amount')
+            ->add('booker')
+            ->add('ad')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
