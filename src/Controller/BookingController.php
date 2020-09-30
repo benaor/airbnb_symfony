08956fede73abb1f1ec23f892afab7d448185a94
benaor <benjamingirard25@gmail.com>
2020-09-30 21:17:27 +0200

    fill the function Book()

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -5,18 +5,37 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Entity\Booking;
 use App\Form\BookingType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class BookingController extends AbstractController
 {
     /**
      * @Route("/ads/{slug}/book", name="booking_create")
+     * @IsGranted("ROLE_USER")
      */
-    public function book(Ad $ad)
+    public function book(Ad $ad, Request $request, EntityManagerInterface $manager)
     {
         $booking = new Booking();
         $form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $booking->setBooker($this->getUser())
+                    ->setAd($ad);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash('success', "Votre réservation a bien été prise en compte.");
+            
+            return $this->redirectToRoute('ads_index', ['id' => $booking->getId()]);
+        }
 
         return $this->render('booking/book.html.twig', [
             'ad' => $ad,
