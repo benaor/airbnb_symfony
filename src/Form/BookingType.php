71cc03a10a033777688c0a7264c0c6e9aeb965c6
benaor <benjamingirard25@gmail.com>
2020-10-04 01:23:42 +0200

    change DateType::class to TextType::class for compatibilty with DatePicker

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,18 +4,19 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use App\Form\ApplicationType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée", "Selectionnez votre date d'arrivée", ["widget" => "single_text"]))
-            ->add('endDate', DateType::class, $this->getConfiguration("Date de départ", "Selectionnez votre date de départ", ["widget" => "single_text"]))
+            ->add('startDate', TextType::class, $this->getConfiguration("Date d'arrivée", "Selectionnez votre date d'arrivée"))
+            ->add('endDate', TextType::class, $this->getConfiguration("Date de départ", "Selectionnez votre date de départ"))
             ->add('comment', TextareaType::class, $this->getConfiguration(false, "si vous souhaitez ajouter un message a votre reservation, c'est ici !", ["required" => false]))
         ;
     }
