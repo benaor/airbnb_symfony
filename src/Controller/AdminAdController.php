5b7c269766bd8c8e5b2a534259d21f629fde2d51
benaor <benjamingirard25@gmail.com>
2020-10-12 13:38:06 +0200

    Manage Ad changes in AdminAdController

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -28,9 +30,21 @@ class AdminAdController extends AbstractController
      * @param Ad $ad
      * @return Response
      */
-    public function edit(Ad $ad){
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager)
+    {
         $form = $this->createForm(AdType::class, $ad);
-        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                "success",
+                "L'annonce <strong>{$ad->getTitle()} </strong> a bien été modifié"
+            );
+        }
+
         return $this->render("admin/ad/edit.html.twig", [
             'ad' => $ad,
             'form' => $form->createView()
