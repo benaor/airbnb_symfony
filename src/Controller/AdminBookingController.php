0f17eb2ea0b7c66f7ad99a879530e2c361140f89
benaor <benjamingirard25@gmail.com>
2020-10-13 12:50:11 +0200

    configure function edit

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminBookingController extends AbstractController
@@ -23,9 +25,21 @@ class AdminBookingController extends AbstractController
     /**
      * @Route("/admin/booking/{id}/edit", name="admin_booking_edit")
      */
-    public function edit(Booking $booking)
+    public function edit(Booking $booking, Request $request, EntityManagerInterface $manager)
     {
         $form = $this->createForm(AdminBookingType::class, $booking);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                "success",
+                "la reservation n°{$booking->getId()} a bien été modifier"
+            );
+            return $this->redirectToRoute("admin_booking_index");
+        }
 
         return $this->render('admin/booking/edit.html.twig', [
             'booking' => $booking,
