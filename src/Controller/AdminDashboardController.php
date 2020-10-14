9bcb18e4bae51226435d206a22bf4c6a4472eaa0
benaor <benjamingirard25@gmail.com>
2020-10-14 17:57:18 +0200

    Create structure HTML for dashboard

---
+++ src/Controller/AdminDashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminDashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index()
+    {
+        return $this->render('admin/dashboard/index.html.twig');
+    }
+}
