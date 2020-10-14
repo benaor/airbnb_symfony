730785ec62c3001f70476738d46f358843aeef4b
benaor <benjamingirard25@gmail.com>
2020-10-14 20:20:20 +0200

    create DQL Query

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,40 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(EntityManagerInterface $manager)
     {
-        return $this->render('admin/dashboard/index.html.twig');
+
+        $ads        = $manager->createQuery("SELECT COUNT(a) FROM App\Entity\Ad a")->getSingleScalarResult();
+        $bookings   = $manager->createQuery("SELECT COUNT(b) FROM App\Entity\Booking b")->getSingleScalarResult();
+        $comments   = $manager->createQuery("SELECT COUNT(c) FROM App\Entity\Comment c")->getSingleScalarResult();
+        $users      = $manager->createQuery("SELECT COUNT(u) FROM App\Entity\User u")->getSingleScalarResult();
+
+        $bestAds    = $manager->createQuery(
+            "SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+            FROM App\Entity\Comment c
+            JOIN c.ad a
+            JOIN a.author u
+            GROUP BY a
+            ORDER BY note DESC"
+        )->setMaxResults(5)
+        ->getResult();
+
+        $badAds    = $manager->createQuery(
+            "SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+            FROM App\Entity\Comment c
+            JOIN c.ad a
+            JOIN a.author u
+            GROUP BY a
+            ORDER BY note ASC"
+        )->setMaxResults(5)
+        ->getResult();
+
+        dump($bestAds);
+
+        return $this->render('admin/dashboard/index.html.twig', [
+            'stats' => compact('ads', 'bookings', 'comments', 'users'),
+            'bestAds' => $bestAds,
+            'badAds' => $badAds
+        ]);
     }
 }
